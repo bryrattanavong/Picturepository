719552bb7fbb93de52ae4a6857671ce750ea030f
bryrattanavong <arandomexample@hotmail.com>
2020-12-30 00:58:27 -0500

    searchbar component

---
+++ src/Components/Searcbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../styles/Searchbar.css';
+const Searchbar: React.FC = () =>{
+    return(
+        <div className="searchbar" id="search">
+            <input type="text"/>
+        </div>
+    )
+}
+export default Searchbar;
